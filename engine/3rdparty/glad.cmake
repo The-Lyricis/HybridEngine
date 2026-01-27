60b84a627e034746a5f7fad6f6da62775ff92905
Pigchick <pigchick1623@gmail.com>
2026-01-27 23:18:45 +0100

    Add glad lib

---
+++ engine/3rdparty/glad.cmake
@@ -0,0 +1,16 @@
+# engine/3rdparty/glad.cmake
+set(glad_SOURCE_DIR_ ${CMAKE_CURRENT_LIST_DIR}/glad_generated)
+
+file(GLOB glad_c_sources CONFIGURE_DEPENDS
+    "${glad_SOURCE_DIR_}/src/*.c"
+)
+
+add_library(glad STATIC ${glad_c_sources})
+
+target_include_directories(glad PUBLIC
+    $<BUILD_INTERFACE:${glad_SOURCE_DIR_}/include>
+)
+
+if(WIN32)
+    target_link_libraries(glad PUBLIC opengl32)
+endif()
