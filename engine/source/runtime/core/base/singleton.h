de36ea62baad1b7fad1d4c8bc4963d5e4363db4a
SaluteBEE <SaluteBEE@outlook.com>
2026-01-28 23:33:34 +0100

    add singleton

---
+++ engine/source/runtime/core/base/singleton.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <type_traits>
+
+namespace Hybrid
+{
+
+    template<typename T>
+    class Singleton
+    {
+    protected:
+        Singleton() = default;
+
+    public:
+        static T& getInstance() noexcept(std::is_nothrow_constructible<T>::value)
+        {
+            static T instance;
+            return instance;
+        }
+        virtual ~Singleton() noexcept = default;
+        Singleton(const Singleton&) = delete;
+        Singleton& operator=(const Singleton&) = delete;
+    };
+}
