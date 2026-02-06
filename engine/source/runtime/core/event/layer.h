0f52e3eed07049d4909673cabe3402af1fd00b0e
Pigchick <pigchick1623@gmail.com>
2026-02-06 13:19:45 +0100

    Add event system/ event layer

---
+++ engine/source/runtime/core/event/layer.h
@@ -0,0 +1,39 @@
+#pragma once
+namespace Hybrid
+{
+    class Layer 
+    {
+    public:
+        Layer(const std::string& name = "Layer") : m_Name(name) {}
+        virtual ~Layer() = default;
+
+        virtual void OnAttach() {}
+        virtual void OnDetach() {}
+        virtual void OnUpdate(float dt) {}
+        virtual void OnEvent(Event& e) {}
+        virtual void OnImGuiRender() {}
+
+        const std::string& GetName() const { return m_Name; }
+    private:
+        std::string m_Name;
+    };
+
+
+    class LayerStack 
+    {
+    public:
+        void PushLayer(Layer* layer);
+        void PushOverlay(Layer* overlay);
+        void PopLayer(Layer* layer);
+        void PopOverlay(Layer* overlay);
+
+        auto begin() { return m_Layers.begin(); }
+        auto end() { return m_Layers.end(); }
+        auto rbegin() { return m_Layers.rbegin(); }
+        auto rend() { return m_Layers.rend(); }
+    private:
+        std::vector<Layer*> m_Layers;
+        size_t m_LayerInsertIndex = 0; // ∆’Õ®≤„≤Â»Îµ„
+    };
+
+}
