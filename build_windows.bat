ff94aef9f1bd8bf214f004a684849bae790d6afd
Pigchick <pigchick1623@gmail.com>
2026-02-19 01:45:40 +0100

    Update build_windows.bat

--- build_windows.bat
+++ build_windows.bat
@@ -14,10 +14,15 @@ set "C_GREEN=!ESC![92m"
 set "C_RED=!ESC![91m"
 
 cls
-echo !C_CYAN!╔══════════════════════════════════════════════════════════════╗!C_RESET!
-echo !C_CYAN!║!C_WHITE!  HYBRID ENGINE  -  BUILD SCRIPT                              !C_CYAN!║!C_RESET!
-echo !C_CYAN!║!C_DIM!  CMake Configure + Build (Release)                           !C_CYAN!║!C_RESET!
-echo !C_CYAN!╚══════════════════════════════════════════════════════════════╝!C_RESET!
+echo !C_CYAN!===========================================================================================!C_RESET!
+echo !C_WHITE!  H   H  Y   Y  BBBB   RRRR   IIIII  DDDD        EEEEE  N   N  GGGG   IIIII  N   N  EEEEE !C_RESET!
+echo !C_WHITE!  H   H   Y Y   B   B  R   R    I    D   D       E      NN  N  G        I    NN  N  E     !C_RESET!
+echo !C_WHITE!  HHHHH    Y    BBBB   RRRR     I    D   D       EEEE   N N N  G  GG    I    N N N  EEEE  !C_RESET!
+echo !C_WHITE!  H   H    Y    B   B  R R      I    D   D       E      N  NN  G   G    I    N  NN  E     !C_RESET!
+echo !C_WHITE!  H   H    Y    BBBB   R  RR  IIIII  DDDD        EEEEE  N   N  GGGG   IIIII  N   N  EEEEE !C_RESET!
+echo !C_CYAN!===========================================================================================!C_RESET!
+echo !C_DIM!  BUILD SCRIPT  -  CMake Configure + Build (Release)!C_RESET!
+echo !C_CYAN!===========================================================================================!C_RESET!
 echo.
 
 echo !C_YELLOW![1/2] Configure...!C_RESET!
